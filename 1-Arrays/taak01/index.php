d926c08501b343378a345108e7498570210d1920
Darimir Mateo <darimir.mateo@student.rocva.nl>
2021-01-25 12:27:38 +0100

    loopswhile1 fixtt!

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,17 @@
+<?php
+
+//$studenten = array("Piet", "Muhammad", "Els", "187",18); 
+$studenten ["voornamen"] = "Muhammad";
+$studenten ["achternaam"] = "Ali";
+$studenten ["klass"] = "187";
+$studenten ["leeftijd"] = "18";
+
+
+echo $studenten["voornamen"] . $studenten["achternaam"] . $studenten["klass"] . $studenten["leeftijd"];
+
+
+//Muhammad Achternaam: Ali Leeftijd: 17 Klas: 9G
+
+
+
+?>
---
+++ 2-Loops/taak01/loopswhile.php
@@ -0,0 +1,21 @@
+<?php
+
+$leeftijd = 1;
+while($leeftijd <=18) {
+    echo " Ik ben $leeftijd, dus ik mag nog niet stemmen<br /> ";
+    $leeftijd++;    
+}
+
+ {
+    $leeftijd = 18;
+    echo " Ik ben $leeftijd, dus ik mag stemmen<br /> ";
+    $leeftijd=18;    
+}
+
+
+
+
+
+
+
+?>
