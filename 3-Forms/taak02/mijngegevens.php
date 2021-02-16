3fdd0e3e8763e5c719423bd4a41fca4c7f2c6be9
Darimir Mateo <darimir.mateo@student.rocva.nl>
2021-02-16 12:29:22 +0100

    php eindopdracht

---
+++ 3-Forms/taak02/mijngegevens.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>gegevens</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<?php
+
+echo $_POST["voornaam"];
+echo $_POST["leeftijd"];
+echo $_POST["adress"];
+echo $_POST["Klass"];
+echo $_POST["plaatsnaam"];
+echo $_POST["favoriete muziekband"];
+
+
+
+?>
+</body>
+</html>
