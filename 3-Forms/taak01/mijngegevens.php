e0f05f9fea9d22b088d790db6ddc5f87245add53
Darimir Mateo <darimir.mateo@student.rocva.nl>
2021-02-26 12:56:13 +0100

    WORKING

--- 3-Forms/taak01/mijngegevens.php
+++ 3-Forms/taak01/mijngegevens.php
@@ -11,7 +11,8 @@ echo $_POST["naam"];
 echo $_POST["lengte"];
 echo $_POST["achternaam"];
 echo $_POST["leeftijd"];
-echo $_POST["favoriete muziekband"];
+echo $_POST["favorietemuziekband"];
+
 
 
 ?>
--- opdracht/advanced-2.php
+++ opdracht/advanced-2.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Eindopdrach PHP ADVANCED</title>
+    <title>gegevens</title>
+    <link rel="stylesheet" href="css/style.css">
+
 </head>
 <body>
-    
+<?php
+
+echo $_POST["naam"];
+echo $_POST["lengte"];
+echo $_POST["achternaam"];
+echo $_POST["leeftijd"];
+echo $_POST["favoriete muziekband"];
 
 
-
+?>
 </body>
 </html>
